fccfee56189f070eb9988d75197ceb6eccbbf6c1
--help <mert.bozkirr@gmail.com>
2024-05-01 21:20:09 +0300

    DSPy summarization

--- app.py
+++ app.py
@@ -6,11 +6,29 @@ from langchain_openai import OpenAIEmbeddings
 import dspy
 import streamlit_scrollable_textbox as stx
 
+#### DSPy
+ollama_model = dspy.OllamaLocal(model='llama3',model_type='chat')
 
-ollama_model = dspy.OpenAI(api_base='http://localhost:11434/v1/', api_key='ollama', model='llama3', stop='\n\n', model_type='chat')
+#gpt4 = dspy.OpenAI(model="gpt-4-turbo-2024-04-09", max_tokens=1000, model_type="chat")
 
-# This sets the language model for DSPy.
 dspy.settings.configure(lm=ollama_model)
+
+#class BasicQA(dspy.Signature):
+ #   """Document summarifying based on llama-3 and DSPy"""
+#
+ #   document = dspy.InputField(desc="A legal opinion document, a written explanation by a judge that accompanies an order or ruling in a case")
+  #  summary = dspy.OutputField(desc="Summarization of legal opinion document")
+
+# generate_answer = dspy.Predict(BasicQA)
+
+# summarize = dspy.ChainOfThought('document -> summary')
+
+class Summarizer(dspy.Signature):
+    """Document summarifying based on llama-3 and DSPy"""
+    document = dspy.InputField(desc="A legal opinion document, a written explanation by a judge that accompanies an order or ruling in a case")
+    summary = dspy.OutputField(desc="Summarization of legal opinion document")
+
+summarize = dspy.Predict(Summarizer)
 
 st.title('Vector Search over Legal Database')
 
@@ -29,5 +47,8 @@ if st.button("🔍"):
 
     st.write(result[0].page_content)
     st.write("source: ", result[0].metadata)
-    stx.scrollableTextbox(result[1].page_content)
-    st.write("source: ", result[1].metadata)
+    response = summarize(document = result[0].page_content)
+    st.write(response.summary)
+    print(response.summary)
+    #stx.scrollableTextbox(result[1].page_content)
+    #st.write("source: ", result[1].metadata, )
