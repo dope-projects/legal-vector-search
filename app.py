b5982582b4de2abe3249c3871d2d579b19b20352
--help <mert.bozkirr@gmail.com>
2024-05-02 03:57:57 +0300

    latest updates, typo fixes

--- app.py
+++ app.py
@@ -4,24 +4,14 @@ import numpy as np
 from langchain_community.vectorstores import Milvus
 from langchain_openai import OpenAIEmbeddings
 import dspy
+import re
 import streamlit_scrollable_textbox as stx
-
+from summarizer import Summarizer
 #### DSPy
+
 ollama_model = dspy.OllamaLocal(model='llama3',model_type='chat')
 
-#gpt4 = dspy.OpenAI(model="gpt-4-turbo-2024-04-09", max_tokens=1000, model_type="chat")
-
 dspy.settings.configure(lm=ollama_model)
-
-#class BasicQA(dspy.Signature):
- #   """Document summarifying based on llama-3 and DSPy"""
-#
- #   document = dspy.InputField(desc="A legal opinion document, a written explanation by a judge that accompanies an order or ruling in a case")
-  #  summary = dspy.OutputField(desc="Summarization of legal opinion document")
-
-# generate_answer = dspy.Predict(BasicQA)
-
-# summarize = dspy.ChainOfThought('document -> summary')
 
 class Summarizer(dspy.Signature):
     """Document summarifying based on llama-3 and DSPy"""
@@ -29,6 +19,9 @@ class Summarizer(dspy.Signature):
     summary = dspy.OutputField(desc="Summarization of legal opinion document")
 
 summarize = dspy.Predict(Summarizer)
+
+
+## STREAMLIT + MILVUS
 
 st.title('Vector Search over Legal Database')
 
@@ -40,15 +33,35 @@ vector_db = Milvus(
     collection_name="LangChainCollection",
 )
 
-query = st.text_input("Search??")
 
-if st.button("🔍"):
-    result = vector_db.similarity_search(query)
 
-    st.write(result[0].page_content)
-    st.write("source: ", result[0].metadata)
-    response = summarize(document = result[0].page_content)
-    st.write(response.summary)
-    print(response.summary)
+instr = 'Ignite ideas..!🔥'
+
+with st.form('chat_input_form'):
+    # Create two columns; adjust the ratio to your liking
+    col1, col2 = st.columns([3,1]) 
+
+    # Use the first column for text input
+    with col1:
+        prompt = st.text_input(
+            instr,
+            value=instr,
+            placeholder=instr,
+            label_visibility='collapsed'
+        )
+    # Use the second column for the submit button
+    with col2:
+        submitted = st.form_submit_button('🔍')
+    
+    if prompt and submitted:
+        result = vector_db.similarity_search(prompt)
+        
+        response = summarize(document = result[0].page_content)
+        st.write("TLDR: ", response.summary)
+
+        st.write(result[0].page_content)
+        st.write(result[0].metadata)
+        
+        print(response.summary)
     #stx.scrollableTextbox(result[1].page_content)
     #st.write("source: ", result[1].metadata, )
